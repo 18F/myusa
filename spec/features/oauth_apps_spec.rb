8b35b280c169f717c6e02f00e243e5cc14af82da
Ryan Rusnak <rusnak@vt.edu>
2014-07-24 15:43:15 -0400

    refactoring oauth tests

--- spec/features/oauth_apps_spec.rb
+++ spec/features/oauth_apps_spec.rb
@@ -1,70 +1,67 @@
 require 'spec_helper'
 
 describe "OauthApps" do
+  let(:user) { create_confirmed_user_with_profile(email: 'first@user.org') }
+  let(:sandbox) do 
+    App.create({name:  'sandbox', custom_text: 'Sandboxy custom message', user_id: user.id, redirect_uri: "http://sandboxhost.com/"})
+  end
   before do
-    @user = create_confirmed_user_with_profile(email: 'first@user.org')
-    @user2 = create_confirmed_user_with_profile(email: 'second@user.org')
-
-    @app_redirect_with_params = App.create(name: 'app_redirect_with_params'){|app| app.redirect_uri = "http://apphost.com?something=true"}
-    @app_redirect_with_params.is_public = true
-    @app_redirect_with_params.save!
-    @app_redirect_with_params.oauth_scopes << OauthScope.top_level_scopes
-    @app_redirect_with_params.oauth_scopes << OauthScope.where('scope_name = "profile.email"').first
-    @app_redirect_with_params_client_auth = @app_redirect_with_params.oauth2_client
-
     @app1 = App.create(name: 'App1', custom_text: 'Custom text for test'){|app| app.redirect_uri = "http://localhost/"; app.url="http://app1host.com"}
     @app1.is_public = true
     @app1.save!
     @app1.oauth_scopes << OauthScope.top_level_scopes
     @app1.oauth_scopes << OauthScope.where('scope_name = "profile.email"').first
     @app1_client_auth = @app1.oauth2_client
-
-    app2 = App.create(name: 'App2'){|app| app.redirect_uri = "http://app2host.com/"}
-    app2.is_public = true
-    app2.save!
-    @app2_client_auth = app2.oauth2_client
-
-    app3 = App.create(name:  'App3'){|app| app.redirect_uri = "http://app3host.com/"}
-    app3.is_public = true
-    app3.save!
-    @app3_client_auth = app3.oauth2_client
-
-    @sandbox = App.create({name:  'sandbox', custom_text: 'Sandboxy custom message', user_id: @user.id, redirect_uri: "http://sandboxhost.com/"})
-    @sandbox_client_auth = @sandbox.oauth2_client
   end
 
+  # let(:app1) do
+  #   App.create(name: 'App1', custom_text: 'Custom text for test') do |a| 
+  #     a.redirect_uri = "http://localhost/"
+  #     a.url="http://app1host.com"
+  #     a.is_public = true
+  #     a.oauth_scopes << OauthScope.top_level_scopes
+  #     a.oauth_scopes << OauthScope.where('scope_name = "profile.email"').first
+  #   end
+  # end
+
+  # before do @app1_client_auth = app1.oauth2_client end
+  
+
   context "when logged in with a user who owns a sandboxed app" do
-    before {login(@user)}
+    before {login(user)}
 
     describe "Authorize sandbox application by owner" do
       it "should ask for authorization and redirect after clicking 'Allow'" do
-        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: @sandbox_client_auth.client_id, redirect_uri: 'http://sandboxhost.com/'))
+        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: sandbox.oauth2_client.client_id, redirect_uri: 'http://sandboxhost.com/'))
         page.should have_content('The sandbox application wants to:')
         click_button('Allow')
         uri = URI.parse(current_url)
         params = CGI::parse(uri.query)
         code = (params["code"] || []).first
-        code.should_not be_empty
+        expect(code).to_not be_empty
       end
 
       it "should log the sandbox application authorization activity, associated with the user" do
         pending "app activity logs not added"
-        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: @sandbox_client_auth.client_id, redirect_uri: 'http://sandboxhost.com/'))
-        page.should have_content('The sandbox application wants to:')
+        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: sandbox.oauth2_client.client_id, redirect_uri: 'http://sandboxhost.com/'))
+        expect(page).to  have_content('The sandbox application wants to:')
         click_button('Allow')
-        @user.app_activity_logs.count.should == 1
-        @user.app_activity_logs.first.app.should == @sandbox
+        expect(user.app_activity_logs.count).to_eq 1
+        expect(user.app_activity_logs.first.app).to_eq @sandbox
       end
     end
   end
 
   context "when logged in with a user who does not own the sandboxed app" do
-    before {login(@user2)}
+    before do
+      @user2 = create_confirmed_user_with_profile(email: 'second@user.org')
+      login(@user2)
+    end
 
     describe "Does not allow sandbox application installation by non owner" do
       it "code in params should not have a value" do
-        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: @sandbox_client_auth.client_id, redirect_uri: 'http://sandboxhost.com/'))
-        page.should have_content("You are accessing an application that doesn't exist or hasn't given you sufficient access.")
+        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: sandbox.oauth2_client.client_id, redirect_uri: 'http://sandboxhost.com/'))
+        expect(page).to  have_content("You are accessing an application that doesn't exist or hasn't given you sufficient access.")
       end
     end
   end
@@ -72,7 +69,7 @@ describe "OauthApps" do
   context "when NON logged in with a user who does not own the sandboxed app" do
     describe "Does not allow sandbox application installation by non owner" do
       it "should present the login page" do
-        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: @sandbox_client_auth.client_id, redirect_uri: 'http://sandboxhost.com/'))
+        visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: sandbox.oauth2_client.client_id, redirect_uri: 'http://sandboxhost.com/'))
         page.should have_content("You need to sign in or sign up before continuing.")
       end
     end
@@ -84,8 +81,8 @@ describe "OauthApps" do
         visit (url_for(controller: 'oauth', action: 'authorize',
                 response_type: 'code', client_id: @app1_client_auth.client_id, redirect_uri: 'http://localhost/')
         )
-        current_path.should eq new_user_session_path
-        page.should have_content('Sign in with Google')
+        (current_path).should == new_user_session_path
+        expect(page).to have_content('Sign in with Google')
       end
     end
 
@@ -94,28 +91,46 @@ describe "OauthApps" do
         visit(url_for(controller: 'oauth', action: 'authorize',
                 response_type: 'code', client_id: 'xyz', redirect_uri: 'http://localhost/')
         )
-        page.should have_content("We're Sorry")
-        page.should have_content("You are accessing an application that doesn't exist or hasn't given you sufficient access.")
+        expect(page).to have_content("We're Sorry")
+        expect(page).to have_content("You are accessing an application that doesn't exist or hasn't given you sufficient access.")
       end
     end
   end
 
   context "when logged in" do
-    before {login(@user)}
+    let(:app_client_auth) do
+      app = App.create(name: 'App1', custom_text: 'Custom text') do |a|
+        a.redirect_uri = 'http://localhost/'
+        a.url = 'http://app1host.com'
+        a.is_public = true
+      end
+      app.save!
+      app.oauth2_client
+    end
+    before {login(user)}
 
     describe "Authorize application" do
 
       it "should log the application authorization activity, associated with the user" do
         pending "app activity logs not added"
         visit(url_for(controller: 'oauth', action: 'authorize', response_type: 'code', client_id: @app1_client_auth.client_id, redirect_uri: 'http://localhost/'))
-        page.should have_content('The App1 application wants to:')
+        expect(page).to have_content('The App1 application wants to:')
         click_button('Allow')
-        @user.app_activity_logs.count.should == 1
-        @user.app_activity_logs.first.app.should == App.find_by_name('App1')
+        expect(user.app_activity_logs.count).to_eq 1
+        expect(user.app_activity_logs.first.app).to_eq App.find_by_name('App1')
       end
     end
 
     describe "Authorize application with scopes" do
+
+      let(:app_redirect_with_params) do 
+        App.create(name: 'app_redirect_with_params') do |ar| 
+          ar.redirect_uri = "http://apphost.com?something=true" 
+          ar.is_public = true
+          ar.oauth_scopes << OauthScope.top_level_scopes
+          ar.oauth_scopes << OauthScope.where('scope_name = "profile.email"').first
+        end
+      end
 
       it "should not allow requests that contain unauthorized scopes" do
         visit(url_for(controller: 'oauth', action: 'authorize',
@@ -125,32 +140,31 @@ describe "OauthApps" do
 
       it "should maintain original redirect_uri parameters (if present) when redirecting with unauthorized scopes error" do
         visit(url_for(controller: 'oauth', action: 'authorize',
-              response_type: 'code', scope: 'profile notifications profile.email profile.address', client_id: @app_redirect_with_params.client_id, redirect_uri: 'http://apphost.com/'))
-        app_redirect_url = URI.parse(@app_redirect_with_params.oauth2_client.redirect_uri)
+              response_type: 'code', scope: 'profile notifications profile.email profile.address', client_id: app_redirect_with_params.client_id, redirect_uri: 'http://apphost.com/'))
+        app_redirect_url = URI.parse(app_redirect_with_params.oauth2_client.redirect_uri)
         app_redirect_url.query.should_not be_nil
-        current_url.should have_content(app_redirect_url.query)
-        current_url.should have_content("error=access_denied")
+        expect(current_url).to have_content(app_redirect_url.query)
+        expect(current_url).to have_content("error=access_denied")
       end
 
 
       it "should ask for authorization and redirect after clicking 'Allow'" do
         visit(url_for(controller: 'oauth', action: 'authorize',
               response_type: 'code', scope: 'profile notifications profile.email', client_id: @app1_client_auth.client_id, redirect_uri: 'http://localhost/'))
-
-        page.should have_content('The App1 application wants to:')
-        page.should have_content('Read your profile information')
-        page.should have_content('Send you notifications')
-        page.should have_content('Read your email address')
-        page.should_not have_content('Read your address')
+        expect(page).to  have_content('The App1 application wants to:')
+        expect(page).to  have_content('Read your profile information')
+        expect(page).to  have_content('Send you notifications')
+        expect(page).to  have_content('Read your email address')
+        expect(page).to_not  have_content('Read your address')
       end
 
       context "when the user does not approve" do
         it "should return an error when trying to authorize" do
           visit(url_for(controller: 'oauth', action: 'authorize',
                 response_type: 'code', scope: 'profile notifications', client_id: @app1_client_auth.client_id, redirect_uri: 'http://localhost/'))
-          page.should have_content('The App1 application wants to:')
-          page.should have_content('Read your profile information')
-          page.should have_content('Send you notifications')
+          expect(page).to  have_content('The App1 application wants to:')
+          expect(page).to  have_content('Read your profile information')
+          expect(page).to  have_content('Send you notifications')
         end
       end
       
@@ -158,12 +172,11 @@ describe "OauthApps" do
         before do
           visit(url_for(controller: 'oauth', action: 'authorize',
                 response_type: 'code', scope: 'profile notifications profile.email', client_id: @app1_client_auth.client_id, redirect_uri: 'http://localhost/'))
-
-          page.should have_content('The App1 application wants to:')
-          page.should have_content('Read your profile information')
-          page.should have_content('Send you notifications')
-          page.should have_content('Read your email address')
-          page.should_not have_content('Read your address')
+          expect(page).to  have_content('The App1 application wants to:')
+          expect(page).to  have_content('Read your profile information')
+          expect(page).to  have_content('Send you notifications')
+          expect(page).to  have_content('Read your email address')
+          expect(page).to_not  have_content('Read your address')
         end
       end
     end

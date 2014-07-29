005c3c929d53bbd2349b81100799f8bd53e52c83
Ryan Rusnak <rusnak@vt.edu>
2014-07-29 16:46:35 -0400

    devise uses appropriate classes or flash notifications and new login screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# ApplicationHelper
 module ApplicationHelper
+  def standard_flash(msg)
+    bootstrap_key_mapping =
+    {
+      'alert' => 'warning',
+      'error' => 'danger',
+      'notice' => 'info'
+    }
+    bootstrap_key_mapping[msg] || msg
+  end
 end

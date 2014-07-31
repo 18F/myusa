e15a93a0049c874388dac8b4f87de954189e6050
Chris Papazian <christopher.papazian@gsa.gov>
2014-07-31 08:49:26 -0700

    remove unused chef roles

--- kitchen/roles/app_server.rb
+++
@@ -1,5 +0,0 @@
-name "app-server"
-description "The app server role for boxit"
-run_list ["myusa-app", "nginx", "nodejs"]
-# run_list ["passenger::install", "passenger::daemon", "nodejs"]
-#env_run_lists "production" => ["recipe[passenger::config_prod]"], "testing" => ["recipe[passenger::config_test]"]
--- kitchen/roles/mysql.rb
+++
@@ -1,6 +0,0 @@
-name "mysql"
-description "Base role for DB support"
-run_list ["mysql::client", "mysql::server"]
-default_attributes  mysql: {
-				              server_root_password: ''
-                    }

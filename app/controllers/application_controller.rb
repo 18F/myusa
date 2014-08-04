800e1e8dcd35229742c5d4769791e612a0d74b04
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-04 09:32:41 -0700

    Create views for index and secret
    Put secret and index in views, rather than `render :text => ...` so that layouts are automatically pulled in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, only: :secret
-  
-  def index
-    render :text => 'Home page'
-  end
-  
-  def secret
-    render :text => 'You got me ' + current_user.email
-  end
+
+  def secret; end
 end

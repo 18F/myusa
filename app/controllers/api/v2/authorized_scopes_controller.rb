7c8a4a1aa55072e89245db3b91d6adaf2eeb083e
Adam Frazin <adam@mobomo.com>
2014-08-06 14:37:56 -0400

    Formatting for Rubocop

--- app/controllers/api/v2/authorized_scopes_controller.rb
+++ app/controllers/api/v2/authorized_scopes_controller.rb
@@ -1,8 +1,15 @@
-class Api::V2::AuthorizedScopesController < Api::ApiController
-	# Authorization not needed because any app calling API can ask for authorized scopes
 
-  def index
-  	scope_list = @token.authorization.try(:scope) || ""
-    render json: scope_list.split(/\s+/)
+module Api
+  module V2
+    # AuthorizedScopesController
+    class AuthorizedScopesController < Api::ApiController
+      # Authorization not needed because any app calling API can ask for
+      # authorized scopes
+
+      def index
+        scope_list = @token.authorization.try(:scope) || ''
+        render json: scope_list.split(/\s+/)
+      end
+    end
   end
 end

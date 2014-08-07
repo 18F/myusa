6dba637a458eb71b804fdaab697c8f95940e9ab4
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-06 17:34:32 -0700

    Remove OauthScope from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-OauthScope.seed_data.each { |os| OauthScope.create os }
+# OauthScope.seed_data.each { |os| OauthScope.create os }

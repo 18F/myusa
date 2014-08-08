6f13fcb56377ee3261130b5c2ca6f73994d10248
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-08 08:37:55 -0700

    Remove `verify_credentials` scope.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -38,7 +38,7 @@ Doorkeeper.configure do
   # default_scopes  :public
   # optional_scopes :write, :update
 
-  optional_scopes *(Profile.scopes + %w(verify_credentials notifications tasks))
+  optional_scopes *(Profile.scopes + %w(notifications tasks))
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then

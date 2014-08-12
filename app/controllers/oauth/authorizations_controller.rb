35e1da708d958eafa9ce3e6abdc143e669ed697d
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-12 11:54:08 -0700

    Minor refactor to profile save in AuthorizationsController

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -4,18 +4,16 @@ class Oauth::AuthorizationsController < Doorkeeper::AuthorizationsController
       current_user.profile.tap do |profile|
         if !profile.update_attributes(profile_params)
           flash[:error] = profile.errors.full_messages.to_sentence
-          redirect_params = params.slice(
-            'client_id', 'redirect_uri', 'state', 'response_type'
-          ).merge(scope: params[:original_scope])
-
-          redirect_to oauth_authorization_path(redirect_params)
+          redirect_to oauth_authorization_path(redirect_back_params)
           return
         end
       end
     end
+
     if params[:scope].is_a?(Array)
       params[:scope] = params[:scope].join(" ")
     end
+
     super
   end
 
@@ -23,5 +21,11 @@ class Oauth::AuthorizationsController < Doorkeeper::AuthorizationsController
 
   def profile_params
     params.require(:profile).permit(Profile::FIELDS + Profile::METHODS)
+  end
+
+  def redirect_back_params
+    params.slice(
+      'client_id', 'redirect_uri', 'state', 'response_type'
+    ).merge(scope: params[:original_scope])
   end
 end

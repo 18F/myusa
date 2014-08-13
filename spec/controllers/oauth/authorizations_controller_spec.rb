ccfa56aa752ab95b9128215116d18fe62f8c3a37
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-13 15:06:15 -0700

    Support legacy token request via authorization endpoint
    legacy implementation used POST /oauth/authorize for both the user facing
    authorization screen and the API endpoint to request a token ... so, we
    have to support it here.

--- spec/controllers/oauth/authorizations_controller_spec.rb
+++ spec/controllers/oauth/authorizations_controller_spec.rb
@@ -9,8 +9,7 @@ describe Oauth::AuthorizationsController do
   let(:client_app) do
     Doorkeeper::Application.create do |a|
       a.name = 'Client App'
-      # Redirect to the 'native_uri' so that Doorkeeper redirects us back to a token page in our app.
-      a.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+      a.redirect_uri = 'http://www.example.com/auth/myusa/callback' #'urn:ietf:wg:oauth:2.0:oob'
       a.scopes = client_application_scopes
       a.owner = user
     end
@@ -26,53 +25,71 @@ describe Oauth::AuthorizationsController do
   end
 
   describe "#create" do
-    let(:raw_token) { 'unique token' }
-    let(:requested_scope) { nil }
-    let(:profile) { nil }
+    context "authorization" do
+      let(:raw_token) { 'unique token' }
+      let(:requested_scope) { nil }
+      let(:profile) { nil }
 
-    before :each do
-      allow(Doorkeeper::OAuth::Helpers::UniqueToken).to receive(:generate) { raw_token }
-    end
+      before :each do
+        allow(Doorkeeper::OAuth::Helpers::UniqueToken).to receive(:generate) { raw_token }
+      end
 
-    before :each do
-      sign_in user
+      before :each do
+        sign_in user
 
-      post :create, {
-        client_id: client_app.uid,
-        redirect_uri: client_app.redirect_uri,
-        state: 'state',
-        response_type: 'code',
-        scope: requested_scope,
-        profile: profile
-      }.compact
-    end
+        post :create, {
+          client_id: client_app.uid,
+          redirect_uri: client_app.redirect_uri,
+          state: 'state',
+          response_type: 'code',
+          scope: requested_scope,
+          profile: profile
+        }.compact
+      end
 
-    shared_examples 'oauth' do
-      it 'creates grant for user' do
-        token = Doorkeeper::AccessGrant.authenticate(raw_token)
-        expect(token).to_not be_expired
+      shared_examples 'oauth' do
+        it 'creates grant for user' do
+          token = Doorkeeper::AccessGrant.authenticate(raw_token)
+          expect(token).to_not be_expired
+        end
+      end
+
+      context 'with string style scopes and no profile' do
+        let(:requested_scope) { %q(profile.email profile.first_name profile.last_name) }
+        it_behaves_like 'oauth'
+      end
+
+      context 'with array style scopes and no profile' do
+        let(:requested_scope) { %w(profile.email profile.first_name profile.last_name) }
+        it_behaves_like 'oauth'
+      end
+
+      context 'with profile' do
+        let(:requested_scope) { %w(profile.email profile.first_name profile.last_name) }
+        let(:profile) { { last_name: 'McTesterson' } }
+        it_behaves_like 'oauth'
+
+        it 'updates profile fields' do
+          expect(user.profile.reload.last_name).to eq('McTesterson')
+        end
       end
     end
 
-    context 'with string style scopes and no profile' do
-      let(:requested_scope) { %q(profile.email profile.first_name profile.last_name) }
-      it_behaves_like 'oauth'
-    end
+    context "token request" do
+      # legacy implementation used POST /oauth/authorize for both the user facing
+      # authorization screen and the API endpoint to request a token ... so, we
+      # have to support it here.
+      it 'redirects to the token path if `grant_type` is passed' do
+        post :create, {
+          client_id: client_app.uid,
+          client_secret: client_app.secret,
+          code: "baz",
+          grant_type: "authorization_code",
+          redirect_uri: client_app.redirect_uri
+        }
 
-    context 'with array style scopes and no profile' do
-      let(:requested_scope) { %w(profile.email profile.first_name profile.last_name) }
-      it_behaves_like 'oauth'
-    end
-
-    context 'with profile' do
-      let(:requested_scope) { %w(profile.email profile.first_name profile.last_name) }
-      let(:profile) { { last_name: 'McTesterson' } }
-      it_behaves_like 'oauth'
-
-      it 'updates profile fields' do
-        expect(user.profile.reload.last_name).to eq('McTesterson')
+        expect(response).to redirect_to(oauth_token_path)
       end
     end
   end
-
 end

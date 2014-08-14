432a3e2a85f9561ea31ba029e4b43ef204573caa
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2014-08-14 16:57:34 -0700

    Use SimpleCov in 'Rails' mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 
 require 'simplecov'
-SimpleCov.start # has to start before application is loaded
+SimpleCov.start 'rails' # has to start before application is loaded
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'

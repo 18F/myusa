2d9019a740b0ce34dc548e8bff427d0dd2b7b2d1
Chris Papazian <christopher.papazian@gsa.gov>
2014-08-15 16:52:42 -0700

    Remove token authentication columns from user
    Since we are using `Rails::cache` to store the email authentication tokens, these are no longer being used.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811170332) do
+ActiveRecord::Schema.define(version: 20140815235128) do
 
   create_table "app_oauth_scopes", force: true do |t|
     t.integer  "app_id"
@@ -230,11 +230,8 @@ ActiveRecord::Schema.define(version: 20140811170332) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
-    t.string   "authentication_token"
-    t.datetime "authentication_sent_at"
   end
 
-  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree

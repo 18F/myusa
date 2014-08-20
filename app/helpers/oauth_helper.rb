e1f6987d488c0acce4dc15f91cff61b0a24373e7
Adam Frazin <adam@mobomo.com>
2014-08-20 14:14:16 -0400

    Begin integrating profile hierarchy

--- app/helpers/oauth_helper.rb
+++ app/helpers/oauth_helper.rb
@@ -1,4 +1,22 @@
 module OauthHelper
+  def scope_groups(scopes_list)
+    # Check for address and address2
+    pre_auth_scopes = scopes_list.to_a
+
+    scopes = insert_extra_scope pre_auth_scopes,
+                                'profile.address',
+                                'profile.address2'
+
+    ordered_scopes = SCOPE_SETS.map { |k| k[1].map { |g| g[:group] } }.flatten
+
+    # Sort array of scopes according to requirements
+    pre_auth_scopes = scopes.sort_by do |x|
+      ordered_scopes.map { |k| k }.flatten.index x
+    end
+
+    @pre_auth_groups = create_groups pre_auth_scopes
+  end
+
   def scope_check_box_tag(scope)
     check_box_tag('scope[]', scope, true,
       id: ('scope_' + scope.gsub(/\./, '_')),
@@ -38,5 +56,31 @@ module OauthHelper
       response_type: pre_auth.response_type,
       scope: pre_auth.scope
     )
+  end
+
+  private
+
+  def insert_extra_scope(arry, first, second)
+    arry.push second if arry.include?(first) && !arry.include?(second)
+    arry
+  end
+
+  def create_groups(pre_auth_scopes)
+    pre_auth_groups = []
+    sets            = {}
+    SCOPE_SETS.keys.each do |set_name|
+      sets[set_name] = []
+      SCOPE_SETS[set_name].each do |set|
+        inter = pre_auth_scopes & set[:group]
+        next if inter.empty?
+        sets[set_name] << {label: set[:label], group: inter}
+      end
+      next if sets[set_name].empty?
+      pre_auth_groups.push(
+        name:   set_name,
+        groups: sets[set_name]
+      )
+    end
+    pre_auth_groups
   end
 end

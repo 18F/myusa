b652986fefa6f0899e69f57a72cf9514b9bd0619
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2014-08-21 16:31:12 -0700

    Add entries for email secrets

--- kitchen/site-cookbooks/myusa/attributes/default.rb
+++ kitchen/site-cookbooks/myusa/attributes/default.rb
@@ -5,6 +5,8 @@ default[:myusa][:user] = 'ubuntu'
 default[:myusa][:group] = 'ubuntu'
 
 default[:myusa][:secrets][:devise_secret_key] = '322a03b72aac1e453a21cec4120d585d0e897fd9d1f41454ae422bb94afe275da162a68ea8ce7a3441dc40a5c091d012651a52b7e5fa7741a0e402d87c483569'
+default[:myusa][:secrets][:aws_ses_username] = ''
+default[:myusa][:secrets][:aws_ses_password] = ''
 
 default[:myusa][:database][:name] = 'myusa_staging' #default[:myusa][:rails_env]
 default[:myusa][:database][:host] = 'localhost'

2cb9fdf120f1e008ae06f3b95ac5b0f1f0c28f29
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2014-08-22 16:14:25 -0700

    Added nginx to vagrant role

--- kitchen/roles/vagrant.rb
+++ kitchen/roles/vagrant.rb
@@ -1,4 +1,4 @@
 name 'all-in-one'
 description 'MyUSA in a box!'
-run_list ['role[all-in-one]']
+run_list ['nginx', 'role[all-in-one]']
 default_attributes 'myusa' => {'user' => 'vagrant', 'group' => 'vagrant'}

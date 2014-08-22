1b79e284ed513e2772f1bac4d393c068c9ed5abd
Joe Polastre <joe@polastre.com>
2014-08-22 00:27:39 +0000

    Fix tests to match new CSS syntax for login

--- spec/support/page_objects/sign_in_page.rb
+++ spec/support/page_objects/sign_in_page.rb
@@ -4,14 +4,14 @@ class SignInPage < SitePrism::Page
   set_url '/users/sign_in'
   set_url_matcher /\/users\/sign_in/
 
-  element :slogan,           	'.slogan'
-  element :email, 				    '.hidden-buttons #inputEmail3'
-  element :google_button,     'button', text: 'Connect with Google'
-  element :remember_me,       '.hidden-buttons #user_remember_me'
-  element :more_options,     	'.more-options'
-  element :more_options_link,	'.more-options a'
-  element :less_options,     	'.less-options'
-  element :less_options_link,	'.less-options a'
-  element :submit, 		'.hidden-buttons input[value="Connect"]'
+  element :slogan,           	'.login h2.text-center'
+  element :email, 				    '.login .hidden-buttons #inputEmail'
+  element :google_button,     '.login button', text: 'Connect with Google'
+  element :remember_me,       '.login .hidden-buttons #user_remember_me'
+  element :more_options,     	'.login .more-options'
+  element :more_options_link,	'.login .more-options a'
+  element :less_options,     	'.login .less-options'
+  element :less_options_link,	'.login .less-options a'
+  element :submit, 		'.login .hidden-buttons input[value="Connect"]'
   element :alert,      '.alert'
 end

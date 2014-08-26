d96eed5b748ba6972e9486f6ec7aa8866a5786fd
Joe Polastre <joe@polastre.com>
2014-08-26 23:47:23 +0000

    Fix typo; remove commented code
    Minor typo in the header.
    
    Commented code was a reference during development, but is
    no longer needed.

--- spec/support/page_objects/sign_in_page.rb
+++ spec/support/page_objects/sign_in_page.rb
@@ -4,17 +4,6 @@ class SignInPage < SitePrism::Page
   set_url '/users/sign_in'
   set_url_matcher /\/users\/sign_in/
 
-  # element :slogan,           	'.slogan'
-  # element :email, 				    '.hidden-buttons #inputEmail'
-  # element :google_button,     'button', text: 'Sign In with Google'
-  # element :remember_me,       '.hidden-buttons #user_remember_me'
-  # element :more_options,     	'.more-options'
-  # element :more_options_link,	'.more-options a'
-  # element :less_options,     	'.less-options'
-  # element :less_options_link,	'.less-options a'
-  # element :submit, 		        '.hidden-buttons input[value="Sign in with MyUSA"]'
-  # element :alert,             '.alert'
-
   element :slogan,            '.login h2.text-center'
   element :email,             '.login .hidden-buttons #inputEmail'
   element :google_button,     '.login button', text: 'Connect with Google'

8439677b5ecb5117c858c00db421d0fc1b93a6e9
Adam Frazin <adam@mobomo.com>
2014-08-28 17:33:54 -0400

    Set page ids to improve tests

--- spec/support/page_objects/profile_page.rb
+++ spec/support/page_objects/profile_page.rb
@@ -6,9 +6,5 @@ class ProfilePage < SitePrism::Page
   set_url '/profile'
   set_url_matcher(/\/profile/)
 
-  elements :profile_properties, '.list-group-item'
-
-  def first_name
-    profile_properties[1]
-  end
+  element :first_name, '#first_name'
 end

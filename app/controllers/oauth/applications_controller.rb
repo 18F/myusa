f88643ab417450809e43ad867ba600a9d09cfc1a
mikefab <mikefabrikant@gmail.com>
2014-09-02 19:27:46 -0400

    Issue 114 Application Create
    Redirects to oauth_authorization_path
    Modal to display app secret and id

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,22 +1,29 @@
 class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
   before_filter :authenticate_user!
-
+  layout "application"
+  
   def index
     @applications = current_user.oauth_applications
   end
 
   def new
-
     super
   end
 
   def create
-
     @application = Doorkeeper::Application.new(application_params)
     @application.owner = current_user
     if @application.save
-      flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :create])
-      respond_with [:oauth, @application]
+
+      flash[:notice] = "<h4>Your application has been created. Please save the below information in safe place.</h4><br>"\
+      "<div class='row'>"\
+      "<div class='col-md-3'> Consumer Public Key  </div><div class='col-md-3'> #{@application.uid} </div>"\
+      "</div>"\
+      "<div class='row'>"\
+      "<div class='col-md-3'> Consumer Secret Key  </div><div class='col-md-3'> #{@application.secret} </div>"\
+      "</div>".html_safe
+
+      redirect_to oauth_applications_path
     else
       render :new
     end
@@ -25,10 +32,8 @@ class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
   private
 
   def application_params
-    app_params = params.require(:application).permit(:name, :description, :image, :scopes, :redirect_uri, :scopes => [])
-    puts app_params.inspect
-    app_params[:scopes] = app_params[:scopes].join(' ')
+    app_params = params.require(:application).permit(:name, :description, :short_description, :custom_text, :url, :image, :scopes, :redirect_uri)
+    app_params[:scopes] = params[:scope] ? params[:scope].join(' ') : []
     app_params
   end
-
 end

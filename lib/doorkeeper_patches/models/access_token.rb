fd3cc5714169f888446a2822ad7c5221c800c5b1
Chris Papazian <christopher.papazian@gsa.gov>
2014-09-03 16:51:45 -0700

    remove TODO comment
    might as well capture all the data. if we need to filter out ip's later we can.

--- lib/doorkeeper_patches/models/access_token.rb
+++ lib/doorkeeper_patches/models/access_token.rb
@@ -1,6 +1,4 @@
 class Doorkeeper::AccessToken
   belongs_to :resource_owner, class_name: User
-  # TODO: do we really want to audit these if they are only created via API
-  # POST?
   audit_on :create
 end

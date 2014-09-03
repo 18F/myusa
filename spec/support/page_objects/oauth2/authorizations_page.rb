87b3b70297cd836eb0b7baff1709cd64eef3e9c6
Ryan Rusnak <rusnak@vt.edu>
2014-09-03 14:31:38 -0400

    changing authorized applications to contain both developer and user applications and use myusa layout

--- spec/support/page_objects/oauth2/authorizations_page.rb
+++ spec/support/page_objects/oauth2/authorizations_page.rb
@@ -13,8 +13,8 @@ module OAuth2
 
   # OAuth2::AuthorizationsPage
   class AuthorizationsPage < SitePrism::Page
-    set_url '/oauth/authorized_applications'
-    set_url_matcher(%r{/oauth/authorized_applications})
+    set_url '/oauth/applications'
+    set_url_matcher(%r{/oauth/applications})
 
     sections :apps, AuthorizedApp, '.panel'
 

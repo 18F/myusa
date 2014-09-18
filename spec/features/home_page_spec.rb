fa178954b07342b56f3d2db15cf48335b782fa00
mikefab <mikefabrikant@gmail.com>
2014-09-18 15:40:29 -0400

    Contact form doesn't work on staging #318
    Test cleanup

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,24 +4,31 @@ describe 'Home Page' do
   let(:user) { FactoryGirl.create(:user) }
   let(:email) { user.email }
   let(:message) { "I'm sold. I want to enter all my profile data in MyUSA!" }
+  let(:display_message) { @home_page.contact_flash }
 
   before :each do
     @home_page = HomePage.new
+    @home_page.submit_contact_form(message)
+  end
+
+  shared_examples 'user contact form' do
+    scenario 'user can contact us' do
+      expect(display_message).to have_content('Thank you. Your message has been sent.')
+    end
   end
 
   context 'without javascript' do
-    it 'displays an alert message' do
-      @home_page.submit_contact_form(message)
+    let(:display_message) { @home_page.contact_flash_no_js }
+
+    it 'Displays message in email' do
       open_email('myusa@gsa.gov')
       expect(current_email).to have_content(message)
-      expect(@home_page.contact_flash_no_js).to have_content('Thank you. Your message has been sent.')
     end
+
+    it_behaves_like 'user contact form'
   end
 
   context 'with javascript', js: true do
-    it 'displays an alert message' do
-      @home_page.submit_contact_form('message')
-      expect(@home_page.contact_flash).to have_content('Thank you. Your message has been sent.')
-    end
+    it_behaves_like 'user contact form'
   end
 end

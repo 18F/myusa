b0e919607dc6c52ded8c5a267e536277bdede122
mikefab <mikefabrikant@gmail.com>
2014-09-22 16:49:41 -0400

    Feature/logo url #344
    Remove paperclip
    Update migrations to handle paperclip fields without using gem

--- db/migrate/20140919145133_remove_paperclip_from_oauth_applications.rb
+++ db/migrate/20140919145133_remove_paperclip_from_oauth_applications.rb
@@ -1,9 +1,15 @@
 class RemovePaperclipFromOauthApplications < ActiveRecord::Migration
   def self.up
-    remove_attachment :oauth_applications, :image
+    remove_column :oauth_applications, :image_file_name
+    remove_column :oauth_applications, :image_content_type
+    remove_column :oauth_applications, :image_file_size
+    remove_column :oauth_applications, :image_updated_at
   end
 
   def self.down
-    add_attachment :oauth_applications, :image
+    add_column :oauth_applications, :image_file_name, :string
+    add_column :oauth_applications, :image_content_type, :string
+    add_column :oauth_applications, :image_file_size, :integer
+    add_column :oauth_applications, :image_updated_at, :datetime
   end
 end

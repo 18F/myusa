31c75dd39b96ed5b86d91c1b0177c6275a8dc149
mikefab <mikefabrikant@gmail.com>
2014-09-23 14:41:47 -0400

    Feature/email app public #354
    Add attachment

--- app/mailers/application_public_mailer.rb
+++ app/mailers/application_public_mailer.rb
@@ -1,10 +1,11 @@
 class ApplicationPublicMailer < ActionMailer::Base
   def notify_is_public(application)
     @to      = 'myusa@gsa.gov'
-    @message = "#{application.name} has gone public."
+    @message = "#{application.name} has requested to become public."
+	attachments.inline['logo.png'] = File.read('app/assets/images/myusa-logo.png')
     mail(
       to: @to,
-      subject: "#{application.name} has gone public"
+      subject: "#{application.name} has requested to become public"
     )
   end
 end

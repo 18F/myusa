6eac85df6638bb53b7918d06aaaf8b90cc9d59d5
mikefab <mikefabrikant@gmail.com>
2014-09-24 12:29:04 -0400

    Email when user requests app to be public #332
    Expand content of email

--- app/mailers/application_public_mailer.rb
+++ app/mailers/application_public_mailer.rb
@@ -1,8 +1,14 @@
 class ApplicationPublicMailer < ActionMailer::Base
-  def notify_is_public(application)
+  def notify_is_public(application, user)
     @to      = 'myusa@gsa.gov'
-    @message = "#{application.name} has requested to become public."
-	attachments.inline['logo.png'] = File.read('app/assets/images/myusa-logo.png')
+    @message = "<p>#{application.name} has requested to become public.</p>"\
+               '<p><u>App owner details</u>'\
+               "<br><br>First name: #{user.profile.first_name}"\
+               "<br>Last name: #{user.profile.last_name}"\
+               "<br>Email: #{user.profile.email}</p>".html_safe
+    attachments.inline['logo.png'] = File.read(
+      'app/assets/images/myusa-logo.png'
+    )
     mail(
       to: @to,
       subject: "#{application.name} has requested to become public"

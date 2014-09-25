8d35e4ab02b4d1b32e4ca18ac6218f029636c75e
Chris Papazian <christopher.papazian@gsa.gov>
2014-09-25 16:48:54 -0700

    Update owner_emails migration to backfill field

--- db/migrate/20140923210054_add_owner_emails_to_oauth_application.rb
+++ db/migrate/20140923210054_add_owner_emails_to_oauth_application.rb
@@ -1,6 +1,18 @@
 class AddOwnerEmailsToOauthApplication < ActiveRecord::Migration
-  def change
+  def up
     add_column :oauth_applications, :owner_emails, :string, :limit => 2000
     add_column :oauth_applications, :developer_emails, :string, :limit => 2000
+
+    Doorkeeper::Application.reset_column_information
+
+    Doorkeeper::Application.all.each do |a|
+      owner_emails = a.owners.map(&:email).join(' ')
+      a.update_column(:owner_emails, owner_emails)
+    end
+  end
+
+  def down
+    remove_column :oauth_applications, :owner_emails
+    remove_column :oauth_applications, :developer_emails
   end
 end

01e14a03704a4bdd788f4046c3ed14dda7eb9330
Ryan Rusnak <rusnak@vt.edu>
2014-09-26 12:08:11 -0400

    updating mailer test

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -11,7 +11,7 @@ describe ContactMailer do
     its(:subject) { should eql 'Contact - MyUSA marketing page' }
     its(:reply_to) { should contain_exactly email }
     # its('body.encoded') { should include message }
-    its(:body) { should include message }
-    its(:body) { should include 'User Name'}
+    its('body.encoded') { should include message }
+    its('body.encoded') { should include 'User Name'}
   end
 end

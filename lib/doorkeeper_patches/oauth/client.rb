65b5a9f18a0ea4be039a512d785d3b68b409dd83
Chris Papazian <christopher.papazian@gsa.gov>
2014-09-30 13:59:59 -0700

    remove commented out code

--- lib/doorkeeper_patches/oauth/client.rb
+++ lib/doorkeeper_patches/oauth/client.rb
@@ -1,11 +1,6 @@
 class Doorkeeper::OAuth::Client
   def valid_for?(user)
     return true if application.public
-
-    # force memberships to reload (TODO: fix this ... )
-    # application.owner_emails = application.owner_emails
-    # application.developer_emails = application.developer_emails
-
     return true if application.owner == user
     return true if application.developer_emails.present? &&
       application.developer_emails.split(' ').include?(user.email)

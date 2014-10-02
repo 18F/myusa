88845915ac04a1cb16ac9ce52b0003ba218f455c
Chris Papazian <christopher.papazian@gsa.gov>
2014-10-02 16:25:00 -0700

    get provider from params instead of passing it as a string

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,31 +3,35 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env['omniauth.auth']
 
     if user = User.find_from_omniauth(auth)
-      log_success(user, 'google_oauth2')
+      log_success(user)
       sign_in_and_redirect user
     elsif user = User.create_from_omniauth(auth)
-      log_success(user, 'google_oauth2')
+      log_success(user)
       sign_in user
       redirect_to new_mobile_recovery_path
     else
-      log_failure(user, 'google_oauth2')
+      log_failure(user)
       flash.alert = 'Unable to connect with Google'
       redirect_to new_user_session_path
     end
   end
 
   def failure
-    log_failure(env['omniauth.error.strategy'].name)
+    log_failure
     super
   end
 
   private
 
-  def log_success(user, provider)
+  def provider
+    params[:provider]
+  end
+
+  def log_success(user)
     UserAction.successful_authentication.create(user: user, data: { authentication_method: provider })
   end
 
-  def log_failure(provider)
+  def log_failure(user=nil)
     UserAction.failed_authentication.create(data: { authentication_method: provider })
   end
 end

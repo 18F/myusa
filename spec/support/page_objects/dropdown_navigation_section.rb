6cdb9da9997baf02c7963f0f01b3ce4bef315a11
Chris Papazian <christopher.papazian@gsa.gov>
2014-10-07 16:03:04 -0700

    add comments explaining redundant app ownership

--- spec/support/page_objects/dropdown_navigation_section.rb
+++ spec/support/page_objects/dropdown_navigation_section.rb
@@ -1,3 +1,5 @@
+require 'site_prism'
+
 class DropdownNavigationSection < SitePrism::Section
   element :profile, "ul li a[text()='Your Profile']"
   element :applications, "ul li a[text()='Your Applications']"

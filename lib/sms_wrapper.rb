f1d3387cc2b345d114e7a18ecfe8cad1001e04ed
Chris Papazian <christopher.papazian@gsa.gov>
2014-10-10 16:34:45 -0700

    Allow email to be used instead of Twilio/SMS in dev for 2FA

--- lib/sms_wrapper.rb
+++ lib/sms_wrapper.rb
@@ -10,6 +10,8 @@ class SmsWrapper
   cattr_accessor :twilio_phone_number
   @@twilio_phone_number = nil
 
+  cattr_accessor :delivery_method
+  @@delivery_method = :email
 
   def self.setup
     yield self
@@ -19,15 +21,23 @@ class SmsWrapper
     @@twilio_client ||= Twilio::REST::Client.new(@@twilio_account_sid, @@twilio_auth_token)
   end
 
-  def send_message(phone_number, body)
+  def send_message(*args)
+    send(:"send_message_#{@@delivery_method}", *args)
+  end
+
+  private
+
+  def send_message_email(*args)
+    Mailer.sms_test_email(*args).deliver
+  end
+
+  def send_message_twilio(phone_number, body)
     @@twilio_client.account.sms.messages.create(
       from: @@twilio_phone_number,
       to: normalize_phone_number(phone_number),
       body: body
     )
   end
-
-  private
 
   def normalize_phone_number(number)
     stripped = number.gsub(/[- \(\)]/, '')
@@ -37,6 +47,15 @@ class SmsWrapper
       "+#{stripped}"
     else
       "+1#{stripped}"
+    end
+  end
+
+  class Mailer < ActionMailer::Base
+    def sms_test_email(phone_number, body)
+      mail(
+        reply_to: phone_number,
+        body: body
+      )
     end
   end
 end

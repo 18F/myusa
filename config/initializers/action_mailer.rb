8420823aebac11b95aac94ef858c6a5ccd18ff7a
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-03-26 14:17:42 -0700

    Use environment variables for app config
    Pulls essential app config from environment variables, cleanly separating config between the chef cookbook and Rails config files.
    Has moved some configuration out from $ENV.rb into initializers.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.default_options = {
-  to: 'MyUSA <myusa@gsa.gov>',
-  reply_to: 'myusa@gsa.gov',
+  to: Rails.configuration.myusa_sender_email,
+  reply_to: Rails.configuration.myusa_sender_email,
   from: 'no-reply@' + ActionMailer::Base.default_url_options[:host]
 }

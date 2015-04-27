38609a4bb7b7718644b9df1db74be13938517c62
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-04-27 11:01:40 -0700

    Slight admin_controller refactor

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,32 +6,36 @@ class AdminController < ApplicationController
   before_filter :require_admin!
 
   def index
-    @applications = Doorkeeper::Application.
-      includes(:authorizations).
-      filter(params[:filter]).
-      search(params[:search]).
-      paginate(page: params[:page], per_page: 8)
+    @applications = Doorkeeper::Application.includes(:authorizations)
 
-    respond_to do |format|      
-      format.html            
-      
+    respond_to do |format|
+      format.html do
+        @applications = @applications.
+              filter(params[:filter]).
+              search(params[:search]).
+              paginate(page: params[:page], per_page: 8)
+      end
+
       format.json do
-        @applications = Doorkeeper::Application.includes(:authorizations).all
-        json_string = {applications: @applications}.as_json(methods: :number_of_authorizations)
+        json_string = {applications: @applications.all}.as_json(methods: :number_of_authorizations)
         render(json: json_string, status: 200)
       end
-      
+
       format.csv do
-        @applications = Doorkeeper::Application.includes(:authorizations).all
-        csv_string = CSV.generate do |csv|
-          csv << Doorkeeper::Application.attribute_names
-          @applications.each do |application|
-            csv << application.attributes.values
-          end
-        end
-        render(text: csv_string, status: 200)
+        render(text: generate_csv(@applications), status: 200)
       end
-      
+
+    end
+  end
+
+  private
+
+  def generate_csv(applications)
+    CSV.generate do |csv|
+      csv << Doorkeeper::Application.attribute_names
+      applications.all.each do |application|
+        csv << application.attributes.values
+      end
     end
   end
 

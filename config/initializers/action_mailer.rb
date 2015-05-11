ba105b85a559d531bbdb37aaec107baa6128618e
Alan deLevie <adelevie@gmail.com>
2015-05-11 16:29:30 -0400

    Added MyUSA to reply_to field for emails.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.default_options = {
   to: 'MyUSA <myusa@gsa.gov>',
   reply_to: 'myusa@gsa.gov',
-  from: 'no-reply@' + ActionMailer::Base.default_url_options[:host]
+  from: 'MyUSA <no-reply@' + ActionMailer::Base.default_url_options[:host] + '>'
 }
